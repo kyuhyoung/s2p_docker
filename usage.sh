3df8195386f1f50a2a37d1bd5062fce7cee0fd5f
Kyuhyoung Choi <kyuhyoung@gmail.com>
2025-05-26 14:00:39 +0900

    Change usage.sh interactively.

--- usage.sh
+++ usage.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -e
-config=/data/data_from_john/jax/jax_214_all_ba_including_config/config.json
+#config=/data/data_from_john/jax/jax_214_all_ba_including_config/config.json
 #config=/data/data_from_john/jax/jax_068_crop_ba_rgb_part/config.json
+
+if [ -n "$1" ]; then
+    config="$1"
+else
+    config=/data/data_from_john/jax/jax_068_crop_ba_rgb_part/config.json
+fi
+
 s2p $config
